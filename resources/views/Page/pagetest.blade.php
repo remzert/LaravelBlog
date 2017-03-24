065fb37227eefa228a3adf0ee7a25509c23d81d4
fullrem38@gmail.com <fullrem38@gmail.com>
2017-03-24 23:49:44 +0100

    changement page de test formulaire ajouter

--- resources/views/Page/pagetest.blade.php
+++ resources/views/Page/pagetest.blade.php
@@ -2,6 +2,23 @@
 
 	@section('content')
         <h1>Hello, htthoho</h1>
+        
+        <form>
+            <div class="form-group">
+                <label name="popopo>">nom</label> <input type="email" class="form-control"/>
+
+            </div>
+
+            <div class="for-group">
+
+                <div class="btn btn-primary">
+                    super bouton
+                </div>
+            </div>     
+      
+        </form>
+       
+             
     
 
 @endsection
