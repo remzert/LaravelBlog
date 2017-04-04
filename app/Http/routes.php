9280ee3f96190f764aa1896e6299308b6b8d329f
fullrem38@gmail.com <fullrem38@gmail.com>
2017-04-04 14:49:02 +0200

    Racourcis URL créé

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,12 @@
 
 Route::get('page/test', 'TestController@index');
 
+Route::resource('link', 'LinksController', ['only' => ['create', 'store']]);
+Route::get('r/{link}',['as' => 'Link.show', 'uses' => 'LinksController@show'])->where('link','[0-9]+');
+
+//Route::get('links/create','LinksController@create');
+//Route::post('links/create','LinksController@store');
+
 
 
 Route::get('/', function () {
